ec4db787d3ca2c97e30b1cbcf95e0c86fc30b864
equitymarkets <derekprice@Dereks-MacBook-Air.local>
2023-01-15 11:14:48 -0500

    added default 0 entry conditional

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,8 +12,8 @@ total_profit = 0
 Cumulative_PnL_change = []
 starter = False
 
-with open('Resources/budget_data.csv') as data_file:
-    csvreader = csv.reader(data_file)
+with open('Resources/budget_data.csv') as input_file:
+    csvreader = csv.reader(input_file)
     next(csvreader)
 
     for row in csvreader:
@@ -30,7 +30,12 @@ with open('Resources/budget_data.csv') as data_file:
         last_profit = int(row[1])
         starter = True
 
-Average_PnL_change = round(sum(Cumulative_PnL_change)/(len(Cumulative_PnL_change)),2)
+#Prevents divide by zero error and alerts user to empty set
+if(total_months == 0):
+    print("You have no records in the set.")
+else:
+    Average_PnL_change = round(sum(Cumulative_PnL_change)/(len(Cumulative_PnL_change)),2)
+
 greatest_decrease = -greatest_decrease 
 
 output_path = os.path.join("analysis", "financial_analysis.csv")
@@ -39,7 +44,7 @@ with open(output_path, 'w') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=',')
     csv_writer.writerow(["Total Months", "Total", "Average Change","Greatest Increase in Profits", "Greatest Decrease in Profits"])
     csv_writer.writerow([total_months, total_profit, Average_PnL_change, (date_greatest_increase, greatest_increase), (date_greatest_decrease, greatest_decrease)])
-    #csv_writer.writerow("a", "B", "c", "d", "d")
+
 print(f"Total Months: {total_months}" )
 print(f"Total: ${total_profit}")
 print(f"Average Change: ${Average_PnL_change}")
