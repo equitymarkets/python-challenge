4518fc3b653241ab20f050d47eb15c418ab5f951
equitymarkets <derekprice@Dereks-MacBook-Air.local>
2023-01-15 13:05:02 -0500

    adding to PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,12 +5,19 @@ print("Election Results")
 print("-------------------------")
 
 total_votes = 0
+candidate_pool = []
+election_winner = "No Name"
 with open("Resources/election_data.csv") as data_file:
     csv_reader = csv.reader(data_file)
     next(csv_reader)
 
     for row in csv_reader:
         total_votes += 1
-
+        if(row[2] not in candidate_pool):
+            candidate_pool.append(row[2])
 print(f"Total Votes Cast: {total_votes}")
+print("-------------------------")
+print(candidate_pool)
+print("-------------------------")
+print(f"Winner: {election_winner}")
 print("-------------------------")
