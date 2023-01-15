6155d3eda6c293141e60396e5592aff66b0568e6
equitymarkets <derekprice@Dereks-MacBook-Air.local>
2023-01-15 00:07:30 -0500

    changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -30,10 +30,18 @@ with open('Resources/budget_data.csv') as data_file:
         last_profit = int(row[1])
         starter = True
 
-Average_PnL_change = sum(Cumulative_PnL_change)/(len(Cumulative_PnL_change))
+Average_PnL_change = round(sum(Cumulative_PnL_change)/(len(Cumulative_PnL_change)),2)
 greatest_decrease = -greatest_decrease 
+
+output_path = os.path.join("analysis", "financial_analysis.csv")
+
+with open(output_path, 'w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',')
+    csv_writer.writerow(["Total Months", "Total", "Average Change","Greatest Increase in Profits", "Greatest Decrease in Profits"])
+    csv_writer.writerow([total_months, total_profit, Average_PnL_change, (date_greatest_increase, greatest_increase), (date_greatest_decrease, greatest_decrease)])
+    #csv_writer.writerow("a", "B", "c", "d", "d")
 print(f"Total Months: {total_months}" )
-print(f"Total: {total_profit}")
-print(f"Average Change: {Average_PnL_change}")
-print(f"Greatest Increase in Profits: {date_greatest_increase} ({greatest_increase})")
-print(f"Greatest Decrease in Profits: {date_greatest_decrease} ({greatest_decrease})")
+print(f"Total: ${total_profit}")
+print(f"Average Change: ${Average_PnL_change}")
+print(f"Greatest Increase in Profits: {date_greatest_increase} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {date_greatest_decrease} (${greatest_decrease})")
